483f9d34837b5a18a2fe2cf5c32a233fe460f0f5
RockThunderheart <alexdaigre@gmail.com>
2018-09-03 00:56:35 -0500

    completed homework

--- DaigreHW2.py
+++ DaigreHW2.py
@@ -3,6 +3,13 @@
 # cs4500
 # Version: Python 3.7
 # Description:
+#   This program simulates a game where the player randomly moves around 
+#   a triangular game board. The player can move in four directions: up left,
+#   up right, down right, down left. Whenever the player moves to a new position
+#   on the board the player markes that positon with a dot. The game ends when
+#   all positions have been marked. As the program runs, the game prints the 
+#   current position. When the game completes, the program prints some stats
+#   about the game that just concluded.
 # External Files:
 # Sources:
 #   https://www.w3schools.com/python/python_classes.asp
@@ -26,6 +33,12 @@ def gameLoop(gameBoard):
 def rollDie():
     return Direction(random.randint(1,4))
 
+# Opens the output file and writes the passed string to it.
+def openOutputFileAndWriteContents(outputData):
+    fileName = "HW2daigreOutfile.txt"
+    with open(fileName, 'w') as outputFile:
+            outputFile.write(outputData)
+
 #Enum to represent the direction of movement on our board
 class Direction(Enum):
     UL = 1
@@ -45,6 +58,7 @@ class GameBoard:
 
     movesLog = ""
 
+    #when class is created log the players starting position
     def __init__(self):
         startPosition = self.getCurrentPositionAsNumber() 
         self.logMoveData(f"{startPosition}, ")
@@ -60,7 +74,7 @@ class GameBoard:
                     break
         if allSpacesVisited == True:
             lastposition = self.getCurrentPositionAsNumber()
-            self.logMoveData(f"{lastposition}.\n")
+            self.logMoveData(f"{lastposition}.\n\n")
         return allSpacesVisited
 
     # Gets the average number of dots on the nodes.
@@ -89,13 +103,14 @@ class GameBoard:
         for subList in self.boardSpaces:
             for space in subList:
                     totalMoves += space
-        return maxDots
+        return totalMoves
     
     # Gets the current position as a sigle number instead of a tuple
     def getCurrentPositionAsNumber(self):
         boardNumbers = [[1],[2,3],[4,5,6],[7,8,9,10],[11,12,13,14,15],[16,17,18,19,20,21]]
         return boardNumbers[self.currentRow][self.currentCollumn]
 
+    #combines the actions of printing and recording log data, for convience.
     def logMoveData(self, dataToLog):
         print(dataToLog, end = "")
         self.movesLog += dataToLog
@@ -142,7 +157,20 @@ class GameBoard:
         return
 
 
-print("a short explanation of the game, and what your simulation is doing \n")
+# Main code
+# Log and print intro info
+logToFile = ""
+programIntro = "This program simulates a game where the player randomly moves around a triangular game board. The player can move in four directions: up left, up right, down right, down left. Whenever the player moves to a new position on the board the player markes that positon with a dot. The game ends when all positions have been marked. As the program runs, the game prints the current position. When the game completes, the program prints some stats about the game that just concluded.\n"
+logToFile += programIntro + "\n"
+print(programIntro)
+# Initialise the gameboard
 gameBoard = GameBoard()
-totalMoves = 0
+# Initialise game loop
 gameBoard = gameLoop(gameBoard)
+# log and print end info
+logToFile += gameBoard.movesLog
+gameStats = f"Total moves: {gameBoard.getTotalMoves()}\nAverage number of dots: {gameBoard.getAverageDots()}\nMaximum number of dots: {gameBoard.getMaxDots()}"
+logToFile += gameStats
+print(gameStats)
+#print loged data to file
+openOutputFileAndWriteContents(logToFile)
